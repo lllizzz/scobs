5b3e4959bf3182f3a427d29bcc0a37d8d8dfb460
Elizaveta <60826293+lllizzz@users.noreply.github.com>
2023-11-05 00:39:12 +0300

    Add files via upload

---
+++ scobs.cpp
@@ -0,0 +1,36 @@
+﻿#include <iostream>
+#include <stack>
+
+using namespace std;
+
+int main()
+{
+    stack<char> s;
+    string str;
+    cin >> str;
+    bool boo = true;
+    for (size_t i = 0; i < str.size(); ++i)
+    {
+        if (str[i] == '{' || str[i] == '[' || str[i] == '(')
+        {
+            s.push(str[i]);
+        }
+        else if (!s.empty() && ((str[i] == '}' && s.top() == '{') || (str[i] == ']' && s.top() == '[') || (str[i] == ')' && s.top() == '(')))
+        {
+            s.pop();
+        }
+        else
+        {
+            boo = false;
+            break;
+        }
+    }
+    if (boo && s.empty())
+    {
+        cout << "YES";
+    }
+    else
+    {
+        cout << "NO";
+    }
+}
